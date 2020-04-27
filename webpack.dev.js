d579d074a800fa517117ffe85cbfc82a38b72ec7
James Hall <james.hall@1stdibs.com>
2020-04-27 15:16:35 -0700

    upgrade webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,24 +1,24 @@
-const merge = require('webpack-merge')
-const webpack = require('webpack')
-const common = require('./webpack.common.js')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const merge = require("webpack-merge");
+const webpack = require("webpack");
+const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   entry: {
-    index: './demo/index.js'
+    index: "./demo/index.js",
   },
   plugins: [
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      title: 'vanillaJS Library Boilerplate',
-      template: './demo/index.html'
-    })
+      title: "js-package-generator",
+      template: "./demo/index.html",
+    }),
   ],
   devServer: {
-    contentBase: './dist',
-    hot: true
-  }
-})
+    contentBase: "./dist",
+    hot: true,
+  },
+});
